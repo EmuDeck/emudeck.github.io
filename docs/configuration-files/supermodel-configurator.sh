a5b6bd705c47cc2e33d41bededc91d87a4918664
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-01-20 13:51:39 -0500

    [Third Party Emulation] Update supermodel to be more flexible

--- docs/configuration-files/supermodel-configurator.sh
+++ docs/configuration-files/supermodel-configurator.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+systemSupermodel="/var/lib/flatpak/app/com.supermodel3.Supermodel/x86_64/stable/active/files/bin"
+userSupermodel="$HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin"
+
 mkdir -p $HOME/.supermodel/Analysis $HOME/.supermodel/Log $HOME/.supermodel/NVRAM
-cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Config $HOME/.supermodel
-cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Assets $HOME/.supermodel
-cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Docs $HOME/.supermodel
+
+    if [ -d $systemSupermodel ]; then
+        cp -r /var/lib/flatpak/app/com.supermodel3.Supermodel/x86_64/stable/active/files/bin/Assets $HOME/.supermodel
+        cp -r /var/lib/flatpak/app/com.supermodel3.Supermodel/x86_64/stable/active/files/bin/Config $HOME/.supermodel
+        cp -r /var/lib/flatpak/app/com.supermodel3.Supermodel/x86_64/stable/active/files/bin/Docs $HOME/.supermodel
+    elif [ -d $userSupermodel ]; then
+        cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Config $HOME/.supermodel
+        cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Assets $HOME/.supermodel
+        cp -r $HOME/.local/share/flatpak/app/com.supermodel3.Supermodel/current/active/files/bin/Docs $HOME/.supermodel
+    else
+        echo "Supermodel install not found"
+    fi
 
 
