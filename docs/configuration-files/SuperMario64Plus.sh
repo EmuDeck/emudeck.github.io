0ec555ce796b06da7e055b5a425544b2154029ce
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-03-29 09:45:59 -0400

    Update SuperMario64Plus.sh: use $HOME instead of hardcoded paths

--- docs/configuration-files/SuperMario64Plus.sh
+++ docs/configuration-files/SuperMario64Plus.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/home/deck/Applications/Distrobox/sm64plus/build/us_pc/sm64.us /home/deck/Applications/Distrobox/sm64plus/build/us_pc/gfx
+
+"$HOME/Applications/Distrobox/sm64plus/build/us_pc/sm64.us" "$HOME/Applications/Distrobox/sm64plus/build/us_pc/gfx"
