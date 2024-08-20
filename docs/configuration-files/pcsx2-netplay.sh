d1c20d8ec28cad4a2726917fe6c1625c7bc843a3
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-08-20 10:47:56 -0400

    PCSX2: Use kdialog

--- docs/configuration-files/pcsx2-netplay.sh
+++ docs/configuration-files/pcsx2-netplay.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-unset GTK_IM_MODULE
 
 while [ "$FINISHED" != "true" ]; do
-    # Prompt for sudo/admin password
-    PASS=$(zenity --title="PCSX2" --width=300 --height=100 --entry --hide-text --text="Enter your sudo password")
+    PASS=$(kdialog --title "PCSX2" --password "Enter your sudo password")
     if [[ $? -ne 0 ]]; then
         exit 1
     fi
 
-    # Check if the password is correct
     if ( echo "$PASS" | sudo -S -k true ); then
         FINISHED="true"
         echo "$PASS" | sudo -S -v
     else
-        zenity --title="PCSX2" --width=150 --height=40 --info --text "Incorrect Password"
+        kdialog --title="PCSX2" --msgbox "Incorrect Password"
     fi
 done
+
 
 echo "$PASS" | sudo "$HOME/Applications/squashfs-root/usr/bin/pcsx2-qt"
 
